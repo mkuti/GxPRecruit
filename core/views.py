e8d5c7a72e846499734ebc5686fc28af64474009
Maripia Kuti <pionste@gmail.com>
2021-06-07 06:27:07 +0100

    Added team section and wired to navbar

--- core/views.py
+++ core/views.py
@@ -6,8 +6,6 @@ from jobs.models import Job
 def home(request):
     '''A view that displays the home page with a search bar'''
     jobs = Job.objects.all()
-    for job in jobs:
-        print(job.job_location)
     filtered_jobs = JobFilter(request.GET, queryset=jobs)
 
     return render(request, 'home.html', {'filter': filtered_jobs})
